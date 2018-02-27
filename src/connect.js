c12a08b37dc6ac291b8dd7a908ceb7da4a0004ab
Peter Shev <piotr.sevcenco@endava.com>
2018-02-27 14:40:25 +0200

    connect bugfix when stream already emmited

--- src/connect.js
+++ src/connect.js
@@ -8,13 +8,17 @@ export const connect = (stateToProps, dispatchToProps) => WrappedComponent => {
   class Connected extends Component {
     constructor(props, context) {
       super(props)
-      if (stateToProps)
+      if (stateToProps) {
+        if (context.store.state$.hasEmitted)
+          this.state = stateToProps(context.store.state$.getValue())
+
         this.subscription = context.store.state$.pipe(
           distinctUntilChanged(),
           map(stateToProps),
           distinctUntilChanged(),
           subscribe(props => this.subscription ? this.setState(props) : this.state = props)
         )
+      }
     }
 
     shouldComponentUpdate(nextProps, nextState) {
